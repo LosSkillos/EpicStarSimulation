94f31d805b24c3fc8342829e63e4aa3552d070bc
LosSkillos <63128140+LosSkillos@users.noreply.github.com>
2021-09-17 20:15:15 +0200

    Lighting attempt

--- trolleclolec.py
+++ trolleclolec.py
@@ -15,25 +15,28 @@ def cmt():
 
 
 
+
 class config:
 	#BASIC SETTINGS
 	fullscreen = True
 	reso_x = 1280
 	reso_y = 720
 	ps = 3	           #player speed
-	sens = .5          #sensitivity of turning when using arrows
+	sens = 1          #sensitivity of turning when using arrows
 	d_sens = sens - sens *2
+	fov = 150
 	
 	#OPTIMIZATION SETTINGS
 	rd = 64	          #roundness of the shperes, applied to both latitude and longitude
-	tl = 1	          #time lock, miliseconds
+	tl = 0	          #time lock, miliseconds
 	opt_dist = 50     #if the star is this far a away, the quality will be devided by opt_mult
 	opt_mult = 4      #if the star is opt_dist far away, the quality will be devided by this
 	
 	#STAR SETTINGS
-	sd = 500   #sphere max distance
-	sm = -500  #sphere min distance
-	sc = 250   #sphere count
+	lighting = False
+	sd = 4000   #sphere max distance
+	sm = -4000  #sphere min distance
+	sc = 500   #sphere count
 	mm = .05   #maximal movement
 	lm = -.05  #minimal movement
 	bs = 8	   #highest sphere size
@@ -51,6 +54,7 @@ lsr = []#Sphere Size
 stx = []#Sphere X Position change per tick
 sty = []#Sphere Y Position change per tick
 stz = []#Sphere Z Position change per tick
+scl = []#Sphere color
 
 
 pos_x = 0
@@ -71,10 +75,23 @@ for i in range(config.sc):
 	stx.append(random.uniform(config.lm, config.mm))
 	sty.append(random.uniform(config.lm, config.mm))
 	stz.append(random.uniform(config.lm, config.mm))
+	x=random.uniform(0.00, 1.00)
+	print(x)
+	scl.append(x)
 
 pygame.init()
 display = (config.reso_x, config.reso_y)
 scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 18)
+
+def update_fps():
+	fps = str(int(clock.get_fps()))
+	fps_text = font.render(fps, 1, pygame.Color("coral"))
+	return fps_text
+
+
 
 if config.fullscreen:
 	pygame.display.toggle_fullscreen()
@@ -85,12 +102,13 @@ glEnable(GL_DEPTH_TEST)
 sphere = gluNewQuadric() #Create new sphere
 
 glMatrixMode(GL_PROJECTION)
-gluPerspective(45, (display[0]/display[1]), 0.1, 5000) #THE LAST ARGUMENT HERE IS THE DRAWING DISTANCE
+gluPerspective(config.fov, (display[0]/display[1]), 0.1, 5000) #THE LAST ARGUMENT HERE IS THE DRAWING DISTANCE
 
 glMatrixMode(GL_MODELVIEW)
 gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
 viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 glLoadIdentity()
+
 
 run = True
 while run:
@@ -141,33 +159,54 @@ while run:
 
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) #Clear the screen
 
-	glPushMatrix()
 
-	sx = sx + .01
-	glTranslatef(sx, sy, sz) #Move to the place
-	glColor4f(1, 1, 1, 1) #Put color
+
+
+	#STAR IN THE MIDDLE
+	glPushMatrix()
+	#sx = sx + .01
+	#glTranslatef(sx, sy, sz) #Move to the place
+	glColor4f(1, 0, 0, 1) #Put color
 	gluSphere(sphere, 1.0, config.rd, config.rd) #Draw sphere
 	glPopMatrix()
 	
+	
+	
+	#THE STARS
 	for n, sp in enumerate(los):
 		glPushMatrix()
+		#MOVEMENT
 		lsx[n] = lsx[n] + stx[n]
 		lsy[n] = lsy[n] + sty[n]
 		lsz[n] = lsz[n] + stz[n]
 		glTranslatef(lsx[n], lsy[n], lsz[n])
-		polc = config.rd
+		#LIGHTING
+		if config.lighting:
+			glEnable(GL_DEPTH_TEST)
+			glEnable(GL_LIGHTING)
+			glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [1, 1, 1, 1.0])
+			glEnable(GL_LIGHT0)
+			glLightfv(GL_LIGHT0, GL_SPOT_EXPONENT, 128)
+			glLightfv(GL_LIGHT0, GL_POSITION, (lsx[n], lsy[n], lsz[n]))
+			#glLightfv
+			glLightfv(GL_LIGHT0, GL_DIFFUSE,  [1, 1, 1, 1.0])
+			glEnable(GL_COLOR_MATERIAL)
+			glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
+		
+
 		
 		#OPTIMIZATION BY DISTANCE
 		#if the sphere is more than 50 away, the program will reduce its render quality by 4
 		#simple yet it made it like 5 times faster
+		polc = config.rd
 		if config.opt_dist < abs(pos_x-lsx[n]) or config.opt_dist < abs(pos_z-lsz[n]):
 			polc = int(polc/config.opt_mult)
-		glColor4f(1, 1, 1, 1) 
+		glColor4f(scl[i], scl[i], scl[i], 1) 
 		gluSphere(los[i], lsr[i], polc, polc)
 		glPopMatrix()
-
-
 	pygame.display.flip() #Update the screen
-	pygame.time.wait(config.tl)  #Wait, propably useless
+	scree.blit(update_fps(), (10,0))
+	if config.tl != 0:
+		pygame.time.wait(config.tl)  #Wait, propably useless
 
 pygame.quit()
